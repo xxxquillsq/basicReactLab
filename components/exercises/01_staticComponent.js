0099b92e125395be7764cdad084d4db6dca32dcc
Qing Sheng <xxxquillsq@gmail.com>
2020-10-21 20:58:03 +0100

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,7 +3,40 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+   <div>
+     <h1>BSc (H) Software Systems Practice - Modules table</h1>
+     <table className="table table-bordered">
+       <thead>
+       <tr>
+         <th>Name</th>
+         <th>No lectures </th>
+         <th>No practicals</th>
+       </tr>
+       </thead>
+       <tbody>
+       <tr>
+         <td>Web App Development</td>
+         <td>3</td>
+         <td>3</td>
+       </tr>
+       <tr>
+         <td>Agile Software Practice</td>
+         <td>3</td>
+         <td>3</td>
+       </tr>
+       <tr>
+         <td>Enterprise Systems Arch</td>
+         <td>2</td>
+         <td>3</td>
+       </tr>
+       <tr>
+         <td>Business Analytics</td>
+         <td>2</td>
+         <td>3</td>
+       </tr>
+       </tbody>
+     </table>
+   </div> 
   );
 };
 
